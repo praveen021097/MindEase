d41308541ffc03ebe1dd22cfa5ddd4b98fd9961c
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2024-01-17 14:13:20 +0530

    homepage complete

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,31 +2,31 @@ const testimonials = [
     {
         name: "Praveen Kumar",
         job: "consultant",
-        image: "praveen.png",
+        image: "./images/praveen.jpg",
         testimonial: "Thanks to OCD relief therapies, my daily struggles have eased. Small, consistent steps have brought immense relief. Grateful for the support and understanding, I'm now managing my thoughts better."
     },
     {
         name: "Emily ",
         job: "developer",
-        image: "praveen.png",
+        image: "./images/ddd.jpg",
         testimonial: "Discovering effective OCD treatments has been life-changing. With professional guidance, my anxiety has lessened, allowing me to regain control and find peace in everyday tasks. A true relief!"
     },
     {
         name: "Alex M",
         job: "manager",
-        image: "praveen.png",
+        image: "./images/alex.jpg",
         testimonial: "OCD relief has been transformative for me. Learning coping mechanisms and embracing mindfulness techniques have significantly improved my daily life. I can now face challenges with resilience and calm."
     },
     {
         name: "Daniel",
         job: "engineer",
-        image: "praveen.png",
+        image: "./images/danial.jpg",
         testimonial: "Incredible relief! Through therapy, I've found tools to navigate my obsessive thoughts. Understanding my triggers and adopting healthier habits has made a world of difference. Grateful for the journey to recovery."
     },
     {
         name: "Sophie",
         job: "CEO Fastwork",
-        image: "praveen.png",
+        image: "./images/sophie.jpg",
         testimonial: "OCD relief has brought back joy and spontaneity into my life. Therapy has equipped me with tools to manage intrusive thoughts, fostering a sense of control and tranquility. A powerful journey towards liberation."
     },
 ];
